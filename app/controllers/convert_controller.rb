a2658e5b1cc02c47857aaa8dc542786143ba9253
Chris Jacob <chris@chrisjacob.us>
2018-11-30 16:29:40 -0600

    change line length, add more replacements

--- app/controllers/convert_controller.rb
+++ app/controllers/convert_controller.rb
@@ -1,9 +1,6 @@
 class ConvertController < ApplicationController
   def convert
-    clean_raw_html = params[:html].gsub!('{{ ', '{{')
-    clean_raw_html.gsub!(' }}', '}}')
-
-    premailer = Premailer.new(clean_raw_html, with_html_string: true)
+    premailer = Premailer.new(params[:html], with_html_string: true, line_length: 120)
 
     converted_html = premailer.to_inline_css
     converted_text = premailer.to_plain_text
@@ -14,6 +11,9 @@ class ConvertController < ApplicationController
     converted_text.gsub!('%7B%7B', '{{')
     converted_text.gsub!('%20%7D%7D', ' }}')
     converted_text.gsub!('%7D%7D', '}}')
+
+    # fix other issues
+    converted_text.gsub!('.html }}}', '.text }}}')
 
     render :json => { html: converted_html, text: converted_text }
   end
